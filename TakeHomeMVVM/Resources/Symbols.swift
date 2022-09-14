8be368fc820737cfe27ee269b9eb3c30b5100c2c
Steven Jemmott <steven@lagomxp.com>
2022-09-14 08:23:44 -0400

    chore: setup resources folder

---
+++ TakeHomeMVVM/Resources/Symbols.swift
@@ -0,0 +1,14 @@
+//
+//  Symbols.swift
+//  TakeHomeMVVM
+//
+//  Created by Steven Jemmott on 14/09/2022.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
---
+++ TakeHomeMVVM/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  TakeHomeMVVM
+//
+//  Created by Steven Jemmott on 14/09/2022.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let text = Color("text")
+    static let pill = Color("pill")
+}
