f89ef174ad2f47043a7fa901e45ae05943876247
Steven Jemmott <steven@lagomxp.com>
2022-09-14 01:29:31 -0400

    Initial Commit

---
+++ TakeHomeMVVM/TakeHomeMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeMVVMApp.swift
+//  TakeHomeMVVM
+//
+//  Created by Steven Jemmott on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
